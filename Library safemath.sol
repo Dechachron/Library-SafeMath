1a41d77f3ba1ec508ee5418ac93cb6d78c8da813
DCN <116252572+Dechachron@users.noreply.github.com>
2022-12-08 14:21:51 +0530

    Update Library safemath.sol

--- Library safemath.sol
+++ Library safemath.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import "github.com/Dechachron/Ownable/blob/main/Ownable.sol";
-
 library SafeMath {
     /**
      * @dev Returns the addition of two unsigned integers, reverting on
